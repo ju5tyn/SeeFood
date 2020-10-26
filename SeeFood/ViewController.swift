c941df626b5b5fb6636e7478201024a36f7d11a5
Justyn <justynhenman@me.com>
2020-10-26 19:23:38 +0000

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,106 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Justyn Henman on 24/07/2020.
+//  Copyright © 2020 Justyn Henman. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    
+    let imagePicker = UIImagePickerController()
+
+    
+    
+    //MARK: - ViewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        
+    }
+    
+
+    //MARK: - IBActions
+    @IBAction func takePhotoTapped(_ sender: UIButton) {
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    
+    //MARK: - Imagepickercontroller
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[.originalImage] as? UIImage{
+        
+            imageView.image = userPickedImage
+            
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert")
+            }
+            
+            detect(image: ciimage)
+            
+            
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    
+    
+    //MARK: - Detect
+    
+    func detect(image: CIImage){
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Broken coreml")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            
+            guard let results = request.results as? [VNClassificationObservation] else{
+                fatalError("vnrequest error")
+            }
+            
+            let result = results.first
+            
+            
+        print(results)
+            
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        
+        do{
+            try handler.perform([request])
+        }catch{
+            print("Error handler")
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
